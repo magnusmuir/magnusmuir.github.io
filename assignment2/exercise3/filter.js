5bdc5797b7b82bde3346940e95adbc72b3ed9a16
magnusmuir <71548732+magnusmuir@users.noreply.github.com>
2022-09-13 12:35:53 +1000

    Add files via upload

--- assignment2/exercise3/filter.js
+++ assignment2/exercise3/filter.js
@@ -1,19 +1,17 @@
 function filterCategory(filterSelection, filterBy){
-    let recipeCards = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
-    // console.log(recipeCards);
-    for(let listItem of recipeCards){
-        // console.log(listItem.dataset.author)
-        if(listItem.dataset.author === filterSelection) {
-            listItem.style.display = "flex";
+    let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
+    for(let listItem of itemsToFilter){
+        if(listItem.dataset[filterSelection] === filterBy){
+            listItem.style.display = "flex"; 
         } else {
             listItem.style.display = "none";
         }
     }
 }
-
-function allCatagories(){
-let recipeCards = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
-for(let listItem of recipeCards){
-    listItem.style.display = "flex";
+function filterAll(filterSelection, filterBy){ 
+    let itemsToFilter = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
+    for(let listItem of itemsToFilter){
+            listItem.style.display = "flex"; 
     }
 }
+
